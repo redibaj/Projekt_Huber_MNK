a49bc123796daa2af10c671358801c21f6434703
emiloetting <emil.oetting@gmx.de>
2023-12-07 17:14:16 +0100

    Player

--- Player.py
+++ Player.py
@@ -1,5 +1,4 @@
-# funny haha
-
+from Board import Board
 class Player():
     def __init__(self, name, number):
         self.name = name
@@ -10,14 +9,18 @@ class Player():
         coordinates_input = (input(f"{self.name}, gib deine Koordinaten ein (Form: x,y): "))
         coordinates_list = coordinates_input.split(",")                      #1. Wert = gewünschtes x, 2. Wert = gewünschtes y
 
-        x_coordinate = (int(coordinates_list[0]) - 1)                       #Wegen Indexierung bei np-Arrays (1. Wert quasi y, 2. Wert quasi x)
+        x_coordinate = (int(coordinates_list[0]) - 1)                        #Wegen Indexierung bei np-Arrays (1. Wert quasi y, 2. Wert quasi x)
         y_coordinate = 5 - int(coordinates_list[1])                  
            
-        if x_coordinate > 5 or y_coordinate > 5:                             #wenn Koordinaten außerhalb des Spielfelds liegen:
+        if x_coordinate >= 5 or y_coordinate >= 5:                           #wenn Koordinaten außerhalb des Spielfelds liegen:
             print("Ungültige Eingabe")
-        
+            self.make_move(board=board)
+        elif x_coordinate < 0 or y_coordinate < 0:
+            print("Ungültige Eingabe")
+            self.make_move(board=board)
         elif board.return_field_value(y_coordinate, x_coordinate) != 0:      #wenn Feld schon belegt:
             print("Feld ist bereits belegt. Lege woanders")
+            self.make_move(board=board)
         else:                                                                #wenn Feld frei:
             board.set_field_value(y_coordinate, x_coordinate, self.number)   #definiert in Board Klasse, lässt Spieler an gewünschter Stelle setzen
         
