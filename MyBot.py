9ba60438b252f94d0a2e2c98585fbf63f1ab80dd
emiloetting <emil.oetting@gmx.de>
2023-12-09 00:13:07 +0100

    Kommis

--- MyBot.py
+++ MyBot.py
@@ -3,26 +3,26 @@ from Board import Board
 import numpy as np
 from random import randint
 
-class MyBotRandom(Player):
+class MyBotRandom(Player):          #erster Bot, der zufälligen Spielzug spielt
     def __init__(self, number):
         self.number = number        #1 oder 2, wird bei make_move() als Setzstein übergeben, Spieler markiert mit seiner Nummer auf dem Feld
 
-    def make_move(self, board):
-        x_coordinate = randint(0,4)
-        y_coordinate = randint(0,4)
-        while board.array[y_coordinate][x_coordinate] != 0:
-            x_coordinate = randint(0,4)
-            y_coordinate = randint(0,4)
+    def make_move(self, board):                                             #lässt Bot spielen, benötigt allerdings eigene make_move Methode, da Verfahren anders
+        x_coordinate = randint(0,4)                                         #generiert eine beliebige x-Koordinate, die auf dem Spielfeld liegen kann
+        y_coordinate = randint(0,4)                                         #generiert eine beliebige y-Koordinate, die auf dem SPielfeld liegen kann
+        while board.array[y_coordinate][x_coordinate] != 0:                 #sollte das Feld auf dem Spielfeld schon belegt (!= 0) sein:
+            x_coordinate = randint(0,4)                                     #andere zufällige x-Koordinate wird generiert
+            y_coordinate = randint(0,4)                                     #andere zufällige y-Koordinate wird generiert
 
-        board.set_field_value(y_coordinate, x_coordinate, self.number)
-        return board.array
+        board.set_field_value(y_coordinate, x_coordinate, self.number)      #methode def. in Board-Klasse, markiert Spielzug auf dem Spielbrett
+        return board.array                                                  #gibt das aktualisierte Spielbrett zurück
 
-class MyBotReactive(Player):
-    def __init__(self, number):
-        self.number = number
-        self.possible_moves = []
+class MyBotReactive(Player):                                                #zweiter Bot, der reaktiv spielt
+    def __init__(self, number):                                         
+        self.number = number                                                #Spielstein, mit dem der Bot seine Markierungen auf Spielfeld Board setzt
+        self.possible_moves = []                                            #Liste, die alle sinnvollen Spielzüge (also Markierungen auf Borad) speichert und aus der später ein Zug ausgeführt werden soll
     
-    def make_random_move(self, board):
+    def make_random_move(self, board):                                      #Methode aus MyBotRandom, ermöglicht zufälligen Spielzug des Bots
         x_coordinate = randint(0,4)
         y_coordinate = randint(0,4)
         while board.array[y_coordinate][x_coordinate] != 0:
@@ -32,18 +32,16 @@ class MyBotReactive(Player):
         board.set_field_value(y_coordinate, x_coordinate, self.number)
         return board.array
         
-    def make_move(self, board):
-        #kein diagonaler Check, weil es muss ja noch Luft nach oben sein :)
-        self.check_horizontally(board)
-        self.check_vertically(board)   
-        if self.possible_moves == []:
-            self.make_random_move(board)
-        else:
-            random_num = randint(0, len(self.possible_moves))
-            board.set_field_value(self.possible_moves[random_num][0], self.possible_moves[random_num][1], self.number)     #sucht aus der Liste mit Tuplen ein zufälliges Tupel aus und setzt den Stein an dieser Stelle
-            return board.array
-        #kein diagonaler Check, weil es muss ja noch Luft nach oben sein :)
-
+    def make_move(self, board):                                             #erneut eigene Methode, da Verfahren auch hier wieder anders zu vorherigen
+                                                                            #kein diagonaler Check auf mögliche Spielzüge (es muss ja noch Luft nach oben sein :))
+        self.check_horizontally(board)                                      #prüft auf spezielle Markierungsmuster auf horizontaler Ebene auf Spielfeld
+        self.check_vertically(board)                                        #prüft auf spezielle Markierungsmuster auf vertikaler Ebene auf Spielfeld
+        if self.possible_moves == []:                                       #sollten keine besorgniserregenden Spielzüge des Gegners, die Eingreifen des Bots bedarfen, vorhanden sein:
+            self.make_random_move(board)                                    #dann soll ein zufälliger Zug gespielt werden
+        else:                                                               #falls es doch eine der in check-Methoden definierten Situationen geben: 
+            random_num = randint(0, len(self.possible_moves))               #generiert eine zufällige Zahl zwischen 0 und der Länge der Liste mit sinnvollen Spielzügen
+            board.set_field_value(self.possible_moves[random_num][0], self.possible_moves[random_num][1], self.number)     #sucht aus der Liste mit sinnvollen Spielzügen durch zuvorige "Zufalls"-Zahl einen Spielzug aus und führt diesen durch
+        
     def check_horizontally(self, board):
         self.possible_moves = []                                                      #um alte Spieloptionen zu löschen
         for row in board.array:                                                       #für jede Reihe in board.array
--- Player.py
+++ Player.py
@@ -1,7 +1,7 @@
 from Board import Board
 class Player():
     def __init__(self, name, number):
-        self.name = name
+        self.name = name            #speichert Name des Spielers
         self.number = number        #1 oder 2, wird bei make_move() als Setzstein übergeben, Spieler markiert mit seiner Nummer auf dem Feld
 
         
@@ -9,23 +9,23 @@ class Player():
         coordinates_input = (input(f"{self.name}, gib deine Koordinaten ein (Form: x,y): "))
         coordinates_list = coordinates_input.split(",")                      #1. Wert = gewünschtes x, 2. Wert = gewünschtes y
 
-        x_coordinate = (int(coordinates_list[0]) - 1)                        #Wegen Indexierung bei np-Arrays (1. Wert quasi y, 2. Wert quasi x)
-        y_coordinate = 5 - int(coordinates_list[1])                  
+        x_coordinate = (int(coordinates_list[0]) - 1)                        #Eingabe Indexierung anders als Rechner -> erstes Feld unten links für User Feld (1,1) statt (0,0)
+        y_coordinate = 5 - int(coordinates_list[1])                          #auch wegen anderer Indexierung des Users. Damit unterste Reihe im Array für Usereingabe die erste Reihe ist
            
-        if x_coordinate >= 5 or y_coordinate >= 5:                           #wenn Koordinaten außerhalb des Spielfelds liegen:
-            print("Ungültige Eingabe")
-            return self.make_move(board=board)
+        if x_coordinate >= 5 or y_coordinate >= 5:                           #wenn Koordinaten außerhalb des Spielfelds (rechts oder oben) liegen:
+            print("Ungültige Eingabe")                                       #signalisiert eine ungültige Eingabe
+            return self.make_move(board=board)                               #ruft Funktion nochmals auf, um erneute Eingabe zu ermöglichen. Return hat David hingepackt um alte Funktion zu schließen
             
-        elif x_coordinate < 0 or y_coordinate < 0:
-            print("Ungültige Eingabe")
-            return self.make_move(board=board)
+        elif x_coordinate < 0 or y_coordinate < 0:                           #wenn Koordinaten unter oder links vom Feld liegen
+            print("Ungültige Eingabe")                                       #signalisiert eine ungültige Eingabe
+            return self.make_move(board=board)                               #ruft Funktion nochmals auf, um erneute Eingabe zu ermöglichen. Return hat David hingepackt um alte Funktion zu schließen
             
-        elif board.return_field_value(y_coordinate, x_coordinate) != 0:      #wenn Feld schon belegt:
-            print("Feld ist bereits belegt. Lege woanders")
-            return self.make_move(board=board)
+        elif board.return_field_value(y_coordinate, x_coordinate) != 0:      #wenn ausgewähltes Feld schon belegt:
+            print("Feld ist bereits belegt. Lege woanders")                  #kommuniziert dem User, dass das Feld bereits belegt ist
+            return self.make_move(board=board)                               #ruft Funktion nochmals auf, um erneute Eingabe zu ermöglichen. Return hat David hingepackt um alte Funktion zu schließen
             
-        else:                                                                #wenn Feld frei:
-            board.set_field_value(y_coordinate, x_coordinate, self.number)   #definiert in Board Klasse, lässt Spieler an gewünschter Stelle setzen
+        else:                                                                #wenn Feld frei ist:
+            board.set_field_value(y_coordinate, x_coordinate, self.number)   #definiert in Board Klasse, lässt Spieler an gewünschter Stelle mit seinem Zeichen (Zahl) setzen
         
         return board.array                                                   #gibt Spielfeld zurück, damit es in Game Klasse verwendet werden kann
                                                                              # -> Runde wird nicht zuende gespielt, wenn erster Spieler gewonnen hat
