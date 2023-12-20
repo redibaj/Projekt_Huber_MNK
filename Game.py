07b4463a61187d52abde97cd9dda36aa4d213e94
Emil Ötting <151923183+emiloetting@users.noreply.github.com>
2023-12-20 19:51:04 +0100

    Bot vs. Bot  möglich! Game.py

--- Game.py
+++ Game.py
@@ -4,13 +4,13 @@ from MyBot import MyBotRandom
 from MyBot import MyBotReactive
 
 class Game:
-    def __init__(self, m=5, n=5, k=4):                                                              #setz m,n und k default-mäßig auf 5,5 und 4 (Spiel funktioniert auch nur so)
+    def __init__(self, m=5, n=5, k=4, player1=None, player2=None):                                                              #setz m,n und k default-mäßig auf 5,5 und 4 (Spiel funktioniert auch nur so)
         self.m = m                                                                                  #definiert m-Wert
         self.n = n                                                                                  #definiert n-Wert
         self.k = k                                                                                  #definiert k-Wert
         self.board = Board()                                                                        #Spielfeld ein Objekt der Klasse Board
-        self.player1 = None                             #Spieler1 = Objekt der Klasse Spieler mit dem Namen, den der Spieler selbst eingibt
-        self.player2 = None                             #Spieler2 = Objekt der Klasse Spieler mit dem Namen, den der Spieler selbst eingibt
+        self.player1 = player1                             #Spieler1 = Objekt der Klasse Spieler mit dem Namen, den der Spieler selbst eingibt
+        self.player2 = player2                             #Spieler2 = Objekt der Klasse Spieler mit dem Namen, den der Spieler selbst eingibt
     
     def start(self):                                                                                #Methode, die vor eigenltichem Spielstart die gewünschten Parameter abfragt
         print("Wilkommen! Wie möchtest du spielen?\nPlayer vs. Player [1] / Player vs. Bot [2]")    #Begrüßung und Wahl der Spielart
@@ -31,7 +31,7 @@ class Game:
                 self.player1 = Player(name=input("Name Spieler 1: "), number=1)                                                                   #wenn schwierigerer Bot (Level 2)
                 self.player2 = MyBotReactive(number=2)                                              #Spieler 2 wird durch Bot Level 2 mit Spielernummer/-markierung 2 ersetzt
                 print()
-                print("Einleitung\nDas Spielfeld besteht aus 5x5 Feldern.\nEin leeres Feld wird durch eine 0 gekennzeichnet, ein belegtes Feld durch eine 1 oder 2.\nSpielt ihr zu zweit, so belegt Spieler 1 das Feld mit einer 1 und Spieler 2 mit einer 2.\nSpielst du alleine, so legst du automatisch die 1 und der Computer die 2.\nDu kannst Werte zwischen 1 und 5 angeben.\nDer erste Wert beschreibt die Horizontale, der zweite die Vertikale\nDie Werte dürfen nicht in einer Klammer stehen!\n")
+                print("Einleitung\nDas Spielfeld besteht aus 5x5 Feldern.\nEin leeres Feld wird durch eine 0 gekennzeichnet, ein belegtes Feld durch eine 1 oder 2.\nSpielt ihr zu zweit, so belegt Spieler 1 das Feld mit einer 1 und Spieler 2 mit einer 2.\nSpielst du alleine, so legst du automatisch die 1 und der Computer die 2.\nDu kannst Werte zwischen 1 und 5 angeben.\nDer erste Wert beschreibt die Horizontale, der zweite die Vertikale\nDie Eingabe ähnelt der bei 'Schiffe versenken'\nDie Werte dürfen nicht in einer Klammer stehen!\n")
                 self.game_loop()                                                                    #game-loop wird gestartet
         
 
@@ -44,6 +44,7 @@ class Game:
             print()
             self.player1.make_move(board=self.board)          #Spieler 1 macht einen Zug
             winner = self.board.has_won()                     #es wird mit Methode aus Board-Klasse überprüft, ob es einen Gewinner gibt (falls ja, würde hier True zurückgegeben werden)
+            full_board = self.board.board_full()
             if winner == True:                                #sollte es einen Gewinner geben:
                 break                                         #Spiel wird beendet
             if full_board == True:                            #prüft, ob das es noch freie Stellen auf dem Spielfeld gibt (falls nicht, wird False zurückgegeben). Falls Feld voll sein sollte: 
@@ -55,6 +56,7 @@ class Game:
             full_board = self.board.board_full()              #schaut, ob das Spielfeld voll ist und speichert aktualisierten Boolean-Return-Wert in Variable full_board
             self.player2.make_move(board=self.board)          #zweiter Spieler setzt
             winner = self.board.has_won()                     #es wird mit Methode aus Board-Klasse überprüft, ob es einen Gewinner gibt (falls ja, würde hier True zurückgegeben und gespeichert werden)
+            full_board = self.board.board_full()
             if winner == True:                                #sollte es einen Gewinner geben (bzw. die vorher definierten, dafür notwendigen Bedigungen erfüllt sein)
                 self.board.display()                          #zeigt ein letztes Mal das Spielfeld an
                 break                                         #beendet Spiel an dieser Stelle
@@ -63,5 +65,37 @@ class Game:
                 break                                         #beendet das Spiel
         print("Spiel vorbei")                                 #sobald Bedingungen der While-Schleife nicht mehr erfüllt sind, geht es hier weiter. Letzte Aktion ist diese Print-Ausgabe
 
-game1=Game()
-game1.start()
+#game1=Game()
+#game1.start()
+
+
+def game_sim(number):
+    winner = []
+    count_bot_1 = 0
+    count_bot_2 = 0
+    count_draw = 0
+    for i in range(number):
+        game=Game(player1=MyBotReactive(number=1), player2=MyBotReactive(number=2))
+        game.game_loop()
+        if game.board.has_won_diagonally() or game.board.has_won_horizontally() or game.board.has_won_vertically():
+            winner.append(game.board.winner)
+        elif game.board.board_full():
+            winner.append(None)    
+    
+    for i in winner:
+        if i == 1:
+            count_bot_1 += 1
+        elif i == 2:
+            count_bot_2 += 1
+        elif i == {1}:
+            count_bot_1 += 1
+        elif i == {2}:
+            count_bot_2 += 1
+        elif i == None:
+            count_draw += 1
+
+    print("Sieger:")
+    print(f"Bot 1: {count_bot_1}\nBot 2: {count_bot_2}\nDraw: {count_draw}")
+
+
+game_sim(1000)
