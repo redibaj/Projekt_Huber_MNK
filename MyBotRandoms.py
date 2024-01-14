e7bbd6e824509541608a2f74580ba4a048630e12
Emil Ötting <151923183+emiloetting@users.noreply.github.com>
2024-01-14 15:20:38 +0100

    Update MyBotRandoms.py

--- MyBotRandoms.py
+++ MyBotRandoms.py
@@ -4,34 +4,47 @@ import numpy as np
 from random import randint
 
 
-class MyBotRandom(Player):          #erster Bot, der zufälligen Spielzug spielt
-    def __init__(self, number):
-        self.number = number        #1 oder 2, wird bei make_move() als Setzstein übergeben, Spieler markiert mit seiner Nummer auf dem Feld
+class MyBotRandom(Player):
+'''Bot, der nur random Züge spielen kann. '''
 
-    def make_move(self, board = Board):                                     #lässt Bot spielen, benötigt allerdings eigene make_move Methode, da Verfahren anders
-        x_coordinate = randint(0,4)                                         #generiert eine beliebige x-Koordinate, die auf dem Spielfeld liegen kann
-        y_coordinate = randint(0,4)                                         #generiert eine beliebige y-Koordinate, die auf dem SPielfeld liegen kann
-        while board.array[y_coordinate][x_coordinate] != 0:                 #sollte das Feld auf dem Spielfeld schon belegt (!= 0) sein:
-            x_coordinate = randint(0,4)                                     #andere zufällige x-Koordinate wird generiert
-            y_coordinate = randint(0,4)                                     #andere zufällige y-Koordinate wird generiert
+    def __init__(self, number):
+        self.number = number        
+
+    def make_move(self, board = Board):  
+    '''ermöglicht dem Bot das setzen seiner Markierung auf dem Spielfeld-Array.
+       2 Pseudo-Zufallszahlen werden erstellt, die für die Indexierung eines Feldes
+       verwendet werden. Ist Feld frei, setzt Bot an dieser Stelle. Andernfalls werden
+       neue Zufallszahlen generiert, bis der Bot ein leeres Feld trifft''' 
+                                        
+        x_coordinate = randint(0,4)                                         
+        y_coordinate = randint(0,4)                                         
+        while board.array[y_coordinate][x_coordinate] != 0:                 #wenn Feld belegt
+            x_coordinate = randint(0,4)                                    
+            y_coordinate = randint(0,4)                                     
 
         board.set_field_value(y_coordinate, x_coordinate, self.number)      #methode def. in Board-Klasse, markiert Spielzug auf dem Spielbrett
-        return board.array                                                  #gibt das aktualisierte Spielbrett zurück
+        return board.array                                                  
 
 
 
+class MyBot2(Player):         
+'''Bot etwas besser als random'''    
 
-class MyBot2(Player):                                                      #zweiter Bot, der zufälligen Spielzug spielt
     def __init__(self, number):
         self.number = number
         self.winning_moves_bot2 = []
 
-    def bot2_make_random_move(self, board):                                      #Methode aus MyBotRandom, ermöglicht zufälligen Spielzug des Bots
+    def bot2_make_random_move(self, board):          
+    '''Bot schaut, ob er mittig auf dem Spielfeld setzen kann. Falls nicht,
+       versucht er, den Rand möglichst zu meiden. Der Bot teilt im Terminal mit,
+       an welcher Stelle er setzt, platziert seine Markierung und übergibt das aktualisierte Spielfeld
+       inklusiver seiner letzten Markierung an die Game-Klasse'''                           
+        
         if board.array[2][2] == 0:
             y_coordinate = 2
             x_coordinate = 2   
         else:                     
-            all_possible_moves = list(np.argwhere(board.array == 0))         # gibt eine Liste von Tupeln zurück, die alle freien Felder (0) besitzen
+            all_possible_moves = list(np.argwhere(board.array == 0))         #gibt eine Liste von Tupeln zurück, die alle freien Felder (0) besitzen
             for tupel in all_possible_moves:  
                 if 0 not in tupel and 4 not in tupel:
                     y_coordinate = tupel[0]
@@ -44,8 +57,6 @@ class MyBot2(Player):                                                      #zwei
         print(f"Bot setzt random hier: {x_coordinate + 1, 5 - y_coordinate}")
         board.set_field_value(y_coordinate, x_coordinate, self.number)                       #methode def. in Board-Klasse, markiert Spielzug auf dem Spielbrett
         return board.array         
-
-    
 
     def make_move(self, board):
         self.check_horizontally(board)
@@ -61,8 +72,8 @@ class MyBot2(Player):                                                      #zwei
             self.winning_moves_bot2 = []
             return board.array
 
-    
-    def check_horizontally(self, board):                     
+    def check_horizontally(self, board):     
+    '''überprüft, ob horizontal auf dem Spielfeld 3 Steine in einer Reihe sind.'''                
         for row_index in range(len(board.array)):       
             row = board.array[row_index]               
             for element in range(len(row)):             
